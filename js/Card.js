5ce939768d65d2ac306ea01b61c52315d4de3b5b
MATHEADS\urajkovic <uros.rajkovic@mathema.de>
2022-03-11 04:58:16 +0100

    new class Card

--- js/Card.js
+++ js/Card.js
@@ -1,3 +1,23 @@
 class Card {
 
+    sing;
+    value;
+
+    constructor(sing, value) {
+        this.sing = sing;
+        this.value = value;
+    }
+    getCrad(){
+        return this.sing + this.value;
+    }
 }
+
+  let cardsAll = [];
+  let cardValue = [2,3,4,5,6,7,8,9,10,"ace","jack","queen","king"];
+  let signs =["clubs","diamonds","hearts","spades"];
+
+  signs.forEach(sign =>{
+      cardValue.forEach(val => {
+          cardsAll.push(new Card(sign,val));
+      })
+  })
