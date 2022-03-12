9694281754f7f4613fcee8cc257b9b79d6de866e
MATHEADS\urajkovic <uros.rajkovic@mathema.de>
2022-03-12 19:59:44 +0100

    class Deck

--- js/Deck.js
+++ js/Deck.js
@@ -1,3 +1,23 @@
 class Deck{
 
+    cards;
+    cardsCopy;
+    constructor(cards){
+        this.cards = cards;
+        this.cardsCopy = [].concat(this.cards);
+    }
+
+    fiveRandomCards() {
+        let fiveRandomCards = [];
+        if(this.cards.length < 6){
+            this.cards = [].concat(this.cards);
+        }
+        for(let i=0;i < 5; i++){
+            let rand = Math.floor(Math.random() * this.cards.length);
+            fiveRandomCards.push(this.cards[rand]);
+            this.cards.slice(rand,1);
+        }
+        return fiveRandomCards;
+    }
 }
+let deck = new Deck(cardsAll);
