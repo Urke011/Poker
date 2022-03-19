03634120686b27e5252a0dccde970c8eed8a34a1
MATHEADS\urajkovic <uros.rajkovic@mathema.de>
2022-03-20 00:56:56 +0100

    class card transform cards

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,20 @@
 class Game {
 
+ constructor(){
+     this.btn =document.querySelector('button');
+     this.cards = document.querySelectorAll(".img_holder");
+ }
+ init(){
+    this.btn.addEventListener('click', ()=> this.flip());
+ }
+ flip(){
+     this.cards.forEach(card=> {
+         let front = card.querySelector('.front');
+         let back = card.children[1];
+         front.style.transform = "perspective(900px) rotateY(0)";
+         back.style.transform = "perspective(900px) rotateY(180deg)";
+     })
+ }
 }
+let game = new Game();
+game.init();
